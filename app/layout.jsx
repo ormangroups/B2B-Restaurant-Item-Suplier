bb40c7851c0ec6aea11bd17e6d2d0b8703ec0d43
Shubham Jena <shubham2jena2003@gmail.com>
2024-12-09 21:25:48 +0530

    static meta data for seo

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,19 @@
-"use client";
+
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
-import { useState } from "react";
 
+export const metadata = {
+  
+  title: 'OrmanIndia',
+  description: 'This Is the Home Page of OrmanIndia ',
+}
 export default function RootLayout({ children }) {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+ 
   return (
     <html lang="en">
       
       <body>
-        <Navbar isLoggedIn={isLoggedIn}/>
+        <Navbar isLoggedIn={true}/>
         <div className="mt-20 min-h-screen">
         {children}
         </div>
