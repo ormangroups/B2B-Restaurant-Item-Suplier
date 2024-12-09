bbe4ded07290988bbf18d9158ac4ed9be34c6db9
Shubham Jena <shubham2jena2003@gmail.com>
2024-12-09 20:39:20 +0530

    Google DNS Removed

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,9 +7,7 @@ export default function RootLayout({ children }) {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
   return (
     <html lang="en">
-      <head>
-      <meta name="google-site-verification" content="XvGMussni5fBo2gQnzEMZuxGDxRRA_advRe2Kkmhmm0" />
-      </head>
+      
       <body>
         <Navbar isLoggedIn={isLoggedIn}/>
         <div className="mt-20 min-h-screen">
