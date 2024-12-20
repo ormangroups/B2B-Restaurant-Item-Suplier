4310bca43649a52411dc447e438fe8b2a4c91aa3
Shubham Jena <shubham2jena2003@gmail.com>
2024-12-20 12:58:58 +0530

    Responsive

--- app/main/favorities/page.jsx
+++ app/main/favorities/page.jsx
@@ -87,7 +87,7 @@ export default function FavoritesPage() {
       <h1 className="text-3xl font-semibold text-gray-800 text-start mb-6">
         Favorites
       </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 mb-3 sm:mb-1 lg:mb-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 mb-4 sm:mb-1 lg:mb-0">
         {favorites.map((item) => (
           <div
             key={item.id}
