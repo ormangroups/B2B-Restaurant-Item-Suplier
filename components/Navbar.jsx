66e0f4b4b9d017817ea9524c0e387ca5895be86d
Shubham Jena <shubham2jena2003@gmail.com>
2024-12-22 14:34:27 +0530

    OPtimization

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,21 +1,30 @@
 "use client";
 import React, { useState } from "react";
-import { FaShoppingCart, FaHeart, FaBell, FaUser, FaTimes, FaBars, FaWallet, FaHistory, FaSignOutAlt } from "react-icons/fa";
+import {
+  FaShoppingCart,
+  FaHeart,
+  FaBell,
+  FaUser,
+  FaTimes,
+  FaBars,
+  FaWallet,
+  FaHistory,
+  FaSignOutAlt,
+} from "react-icons/fa";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import "../styles/global.css";
 import Cookies from "js-cookie";
 import { MdSchedule } from "react-icons/md";
-import { clearUserData } from "../redux/slices/userSlice";
-import { clearRestaurantDetails } from "../redux/slices/restaurantSlice";
 
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
   const [activeButton, setActiveButton] = useState("shop"); // Default active button
-  const [cartQuantity, setCartQuantity] = useState(4); // Example cart quantity
-  const [hasNotifications, setHasNotifications] = useState(true); // Example notification state
+  const [cartQuantity] = useState(4); // Example cart quantity
+  const [hasNotifications] = useState(true); // Example notification state
+  const [isLoggedIn, setIsLoggedIn] = useState(true); // State to handle login status
   const router = useRouter();
-  const dispatch = useDispatch();
 
   // Get role and user data from Redux store
   const role = useSelector((state) => state.user.role);
@@ -30,14 +39,13 @@ function Navbar() {
   const handleLogin = () => router.push("/login");
   const openCart = () => router.push("/main/cart");
   const goToMain = () => router.push("/main");
-  const openFavorite = () => router.push("/main/favorities");
+  const openFavorite = () => router.push("/main/favorites");
   const openNotification = () => router.push("/main/notifications");
 
   const handleLogout = () => {
     Cookies.remove("userData");
     Cookies.remove("restaurantData");
-    dispatch(clearUserData());
-    dispatch(clearRestaurantDetails());
+    setIsLoggedIn(false); // Update the state to re-render the component
     router.push("/");
   };
 
@@ -45,7 +53,7 @@ function Navbar() {
   if (role === "ADMIN") return null;
 
   // If user is not logged in, render only login button
-  if (role === null) {
+  if (!isLoggedIn || role === null) {
     return (
       <div className="fixed top-0 left-0 right-0 bg-white shadow-md z-30 flex justify-between items-center px-6 py-4">
         <Link href="/">
@@ -89,7 +97,9 @@ function Navbar() {
                 handleButtonClick("shop");
                 goToMain();
               }}
-              className={`flex items-center space-x-2 ${activeButton === "shop" ? "text-red-500" : "text-gray-700"} hover:text-red-500`}
+              className={`flex items-center space-x-2 ${
+                activeButton === "shop" ? "text-red-500" : "text-gray-700"
+              } hover:text-red-500`}
             >
               <FaShoppingCart className="text-xl" />
               <span>Shop</span>
@@ -99,7 +109,9 @@ function Navbar() {
                 handleButtonClick("favorite");
                 openFavorite();
               }}
-              className={`flex items-center space-x-2 ${activeButton === "favorite" ? "text-red-500" : "text-gray-700"} hover:text-red-500`}
+              className={`flex items-center space-x-2 ${
+                activeButton === "favorite" ? "text-red-500" : "text-gray-700"
+              } hover:text-red-500`}
             >
               <FaHeart className="text-xl" />
               <span>Favorite</span>
@@ -109,10 +121,14 @@ function Navbar() {
                 handleButtonClick("notification");
                 openNotification();
               }}
-              className={`relative flex items-center space-x-2 ${activeButton === "notification" ? "text-red-500" : "text-gray-700"} hover:text-red-500`}
+              className={`relative flex items-center space-x-2 ${
+                activeButton === "notification" ? "text-red-500" : "text-gray-700"
+              } hover:text-red-500`}
             >
               <FaBell className="text-xl relative">
-                {hasNotifications && <span className="absolute top-0 right-0 bg-red-500 w-2 h-2 rounded-full"></span>}
+                {hasNotifications && (
+                  <span className="absolute top-0 right-0 bg-red-500 w-2 h-2 rounded-full"></span>
+                )}
               </FaBell>
               <span>Notify</span>
             </button>
@@ -121,15 +137,12 @@ function Navbar() {
                 handleButtonClick("cart");
                 openCart();
               }}
-              className={`relative flex items-center space-x-2 ${activeButton === "cart" ? "text-red-500" : "text-gray-700"} hover:text-red-500`}
+              className={`relative flex items-center space-x-2 ${
+                activeButton === "cart" ? "text-red-500" : "text-gray-700"
+              } hover:text-red-500`}
             >
               <div className="relative">
                 <FaShoppingCart className="text-xl" />
-                {cartQuantity > 0 && (
-                  <span className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-4 h-4 flex items-center justify-center text-xs">
-                    {cartQuantity}
-                  </span>
-                )}
               </div>
               <span>Cart</span>
             </button>
@@ -144,7 +157,10 @@ function Navbar() {
       {menuOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-75 z-40 flex justify-center items-center">
           <div className="relative bg-white w-4/5 h-4/5 md:w-1/2 md:h-3/4 rounded-lg shadow-lg overflow-hidden">
-            <button className="absolute top-4 right-4 text-gray-700" onClick={toggleMenu}>
+            <button
+              className="absolute top-4 right-4 text-gray-700"
+              onClick={toggleMenu}
+            >
               <FaTimes className="text-3xl hover:text-red-500 transition duration-200" />
             </button>
             <div className="p-6">
@@ -153,45 +169,54 @@ function Navbar() {
               </h2>
               <ul className="space-y-6">
                 <li>
-                  <Link href="/main/profile" className="flex items-center text-gray-700 hover:text-red-500" onClick={toggleMenu}>
+                  <Link
+                    href="/main/profile"
+                    className="flex items-center text-gray-700 hover:text-red-500"
+                    onClick={toggleMenu}
+                  >
                     <FaUser className="mr-4 text-xl" /> Outlet Information
                   </Link>
                 </li>
                 <li>
-                  <Link href="/main/payments" className="flex items-center text-gray-700 hover:text-red-500" onClick={toggleMenu}>
+                  <Link
+                    href="/main/payments"
+                    className="flex items-center text-gray-700 hover:text-red-500"
+                    onClick={toggleMenu}
+                  >
                     <FaWallet className="mr-4 text-xl" /> Payment Settlement
                   </Link>
                 </li>
                 <li>
-                  <Link href="/main/orders" className="flex items-center text-gray-700 hover:text-red-500" onClick={toggleMenu}>
+                  <Link
+                    href="/main/orders"
+                    className="flex items-center text-gray-700 hover:text-red-500"
+                    onClick={toggleMenu}
+                  >
                     <FaHistory className="mr-4 text-xl" /> Order History
                   </Link>
                 </li>
                 <li>
-                  <Link href="/main/schedule" className="flex items-center text-gray-700 hover:text-red-500" onClick={toggleMenu}>
+                  <Link
+                    href="/main/schedule"
+                    className="flex items-center text-gray-700 hover:text-red-500"
+                    onClick={toggleMenu}
+                  >
                     <MdSchedule className="mr-4 text-xl" /> Daily Orders
                   </Link>
                 </li>
                 <li>
                   <button
-            className="flex items-center text-gray-700 hover:text-red-500 w-full text-left"
-            onClick={() => {
-              Cookies.remove("userData");
-              Cookies.remove("restaurantData");
-              router.push("/");
-              toggleMenu();
-            }}
-          >
-            <FaSignOutAlt className="mr-4 text-xl" /> Logout
-          </button>
-        </li>
-      </ul>
-    </div>
-  </div>
-</div>
-
-)}
-
+                    className="flex items-center text-gray-700 hover:text-red-500 w-full text-left"
+                    onClick={handleLogout}
+                  >
+                    <FaSignOutAlt className="mr-4 text-xl" /> Logout
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      )}
 
       {/* Bottom Navbar for Small Screens */}
       <div className="fixed bottom-0 left-0 right-0 bg-white shadow-inner flex justify-around items-center px-4 py-2 lg:hidden z-40">
@@ -200,7 +225,9 @@ function Navbar() {
             handleButtonClick("shop");
             goToMain();
           }}
-          className={`flex flex-col items-center ${activeButton === "shop" ? "text-red-500" : "text-gray-700"} hover:text-red-500`}
+          className={`flex flex-col items-center ${
+            activeButton === "shop" ? "text-red-500" : "text-gray-700"
+          } hover:text-red-500`}
         >
           <FaShoppingCart className="text-xl" />
           <span className="text-xs">Shop</span>
@@ -210,7 +237,8 @@ function Navbar() {
             handleButtonClick("favorite");
             openFavorite();
           }}
-          className={`flex flex-col items-center ${activeButton === "favorite" ? "text-red-500" : "text-gray-700"} hover:text-red-500`}
+          className={`flex flex-col items-center ${
+            activeButton === "favorite" ? "text-red-500" : "text-gray-700"} hover:text-red-500`}
         >
           <FaHeart className="text-xl" />
           <span className="text-xs">Favorite</span>
